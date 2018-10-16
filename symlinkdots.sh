fe3c51c913861d00018425cf0d2031480b0ae06c
Grant Rolls <grantr90@gmail.com>
2018-10-16 22:18:39 +1100

    updated dotfiles dir to make git

--- symlinkdots.sh
+++ symlinkdots.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=~/dots                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bash_aliases vimrc"    # list of files/folders to symlink in homedir
 
